9c5186817ebb99f269b76361e65289248e7f214c
Ashish <ashish74624@gmail.com>
2024-11-26 00:03:19 +0530

    Long and short term forecast merged

--- energy_forecast_project/forecasting/views.py
+++ energy_forecast_project/forecasting/views.py
@@ -6,13 +6,12 @@ import pandas as pd
 from django.conf import settings
 import subprocess
 
-
-
 def forecast_view(request):
-    prediction = None
+    short_term_prediction = None
+    long_term_prediction = None
+    
     if request.method == 'POST':
         date = request.POST.get('date')
-        model_choice = request.POST.get('model')
         data_path = os.path.join(settings.BASE_DIR, 'data/processed/weather_and_consumption.csv')
         df = pd.read_csv(data_path, index_col=0, parse_dates=True)
 
@@ -24,14 +23,19 @@ def forecast_view(request):
         except subprocess.CalledProcessError as e:
             print(f"Error while executing scripts: {e}")
 
-        if model_choice == 'short':
-            model = ShortTermEnergyModel(df)
-        elif model_choice == 'long':
-            model = LongTermEnergyModel(df)
-        else:
-            model = None
+        # Short-term prediction
+        short_term_model = ShortTermEnergyModel(df)
+        short_term_prediction = short_term_model.predict_for_date(date)
 
-        if model:
-            prediction = model.predict_for_date(date)
-    
-    return render(request, 'forecasting/forecast.html', {'prediction': prediction})
+        # Long-term prediction
+        long_term_model = LongTermEnergyModel(df)
+        long_term_prediction = long_term_model.predict_for_date(date)
+
+    return render(
+        request,
+        'forecasting/forecast.html',
+        {
+            'short_term_prediction': short_term_prediction,
+            'long_term_prediction': long_term_prediction
+        }
+    )
